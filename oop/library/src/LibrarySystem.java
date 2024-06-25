fa558e1d35f1b4f89f8befaa6fef8dbdc7bfa034
Navachander Navasantar <navachander@graduate.utm.my>
2024-06-25 23:03:31 +0800

    Delete oop/library directory

--- oop/library/src/LibrarySystem.java
+++
@@ -1,7 +0,0 @@
-public class LibrarySystem {
-    public static void main(String[] args) {
-        Library library = new Library();
-        LibraryUI libraryUI = new LibraryUI(library);
-        libraryUI.showMainMenu();
-    }
-}
--- oop/library/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Library library = new Library();
-        LibraryUI libraryUI = new LibraryUI(library);
-        libraryUI.showMainMenu();
-    }
-}
